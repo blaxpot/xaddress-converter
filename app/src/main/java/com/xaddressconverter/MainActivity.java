0653c45638d7a8e0cc8d9c88bea14b4910508804
Conal Moloney <blaxpot@blax.site>
2024-01-03 20:00:54 +1100

    Add a default activity

---
+++ app/src/main/java/com/xaddressconverter/MainActivity.java
@@ -0,0 +1,16 @@
+package com.xaddressconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.xaddressconverter.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
